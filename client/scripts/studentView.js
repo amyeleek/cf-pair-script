d811d26feb2a9554635eda678d37754b8a3825a8
Amy Leek <amyeleek@gmail.com>
2016-06-01 16:38:27 -0700

    First pass for React

--- client/scripts/studentView.js
+++ client/scripts/studentView.js
@@ -23,7 +23,7 @@
 */
 
 (function(module){
-	studentView = {};
+	var studentView = {};
 
 	//helper function?
 	studentView.clearData = function(ele){
@@ -36,11 +36,11 @@
 	}
 
 	//runs first
-	studentView.populateStudents = function(){
-		students.forEach(function(student){
-			studentView.showTemplate('student', 'students', student);
-		})
-	}
+	// studentView.populateStudents = function(){
+	// 	students.forEach(function(student){
+	// 		studentView.showTemplate('student', 'students', student);
+	// 	})
+	// }
 
 	//Consider: Make two buttons. One to store the students (with current paired with),
 	// another to run the algorithm again without updating the paired with array
@@ -79,12 +79,98 @@
 		if($('#students li') === []) $('#pop').hide();
 	}
 
+	studentView.renderReact = function(){
+		ReactDOM.render(
+			<StudentList />, 
+			document.getElementById('students')
+		);
+	}
+
 	studentView.init = function(){
 		studentView.buttonsHandler();
 		studentView.expHandler();
-		studentView.populateStudents();
+		// studentView.populateStudents();
 		studentView.hidePopulate();
+		studentView.renderReact();
 	}
+
+	//react
+
+	var StudentList = React.createClass({
+		render: function() {
+			console.log(students);
+			var showStudents = students.map(function(student){
+				return (
+					<ViewStudent key={student._id} name={student.name} exp={student.exp} 
+								 driverCount={student.driverCount} pairedWith={student.pairedWith}>
+					</ViewStudent>
+				);
+			});
+			return (
+				<ul className="students">
+					{showStudents}
+				</ul>
+			);
+		}
+	});
+
+	//Consider: Make subcombponents of name and pairedWith
+	//hide/show paired with
+	var ViewStudent = React.createClass({
+		getInitialState: function(){
+			return(
+				{exp: this.props.exp}
+			)
+		},
+		handleExpChange: function(e){
+			this.setState({exp: e.target.value});
+			Student.updateExp(this.props.name, e.target.value);
+		},
+		changeName: function(){
+			return(
+				<input onChange={this.handleNameChange} placeholder={this.props.name} value={this.props.name}></input>
+			)
+		},
+		render: function() {
+			var showPairedWith = this.props.pairedWith.map(function(pair, i){
+				return (
+					<li key={i}>{pair}</li>
+				);
+			});
+
+			// var showName;
+			// if(nameChange){
+			// 	showName = <StudentNameForm />
+			// }else{
+			// 	showName = <StudentName />
+			// }
+
+			return (
+				<li><StudentName name={this.props.name} />, Experience: {this.state.exp}  
+	        	<select class="exp" onChange={this.handleExpChange} value={this.state.exp}>	
+	        		<option value="change">Change</option>	
+	        	 	<option value="1">1</option>
+	        	 	<option value="2">2</option>
+	        	 	<option value="3">3</option>
+	        	</select>
+	        	Driver count: {this.props.driverCount}, Previously paired with: <ul>{showPairedWith}</ul></li>);
+		}
+	});
+
+	var StudentName = React.createClass({
+		render: function(){
+			return (<span>Name: {this.props.name}</span>)
+		}
+	});
+
+	var StudentNameForm = React.createClass({
+		render: function(){
+			return (
+				<input type="text" placeholder={this.props.name}
+          			   value={this.state.name} onChange={this.handleNameChange} /> 
+			)
+		}
+	});
 
 	//decouple pair-making from the view
 	$(document).ready(Student.getStudents(studentView.init));
